1dc906e0c1e2dbada28a74d1583f6719743513ca
Will Warren <will.warren04@gmail.com>
2017-04-26 14:08:58 -0400

    heroku heroku

--- masterpeace/settings.py
+++ masterpeace/settings.py
@@ -125,6 +125,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'masterpeace.urls'
