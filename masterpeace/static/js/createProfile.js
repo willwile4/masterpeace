ed3c3eebc9a2426aa152aaeff9b51e1d5983aa1c
Will Warren <will.warren04@gmail.com>
2017-04-29 15:37:21 -0400

    refactored createProfile.js

--- masterpeace/static/js/createProfile.js
+++ masterpeace/static/js/createProfile.js
@@ -1,15 +1,14 @@
-console.log('js works here');
+//js file to create a profile via ajax. users are directed here upon registration.
+//known bug: any user can update any profile 4/28
 
 function createProfile(e) {
     e.preventDefault();
-    console.log('hi mom');
     var post_data = { 'bio': $('#bio').val(),
                       'allow_messages': false, //$('#allowMessages').val(),
                       'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
                       'user': $('#user_id').val(),
                       'user_id': $('#user_id').val(),
         };
-    console.log(post_data);
     var settings = {
         method: "POST",
         url: '/api/profile/',
