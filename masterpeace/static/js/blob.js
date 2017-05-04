7446400347836d049b442aee4c0b8370f840c4d9
Will Warren <will.warren04@gmail.com>
2017-05-04 14:19:15 -0400

    we now are able to store images as files and view them on our page. the art gallery has art.

--- masterpeace/static/js/blob.js
+++ masterpeace/static/js/blob.js
@@ -3,28 +3,14 @@
 
 function textToImg(text) {
   //get ascii string into binary then into an array then into a blob.
-  //had some strange issues using ArrayBuffer()
-  console.log(document.getElementById('fileDisplayArea'));
-  var file = new Array(atob(document.getElementById('fileDisplayArea').innerText));
-  file = new Blob(file, {type:'image/*'});
-  console.log(file);
-  //currently doesn't seem to be loading as a DataURL. It's type is string and shows up as a broken image.
-  var reader = new FileReader();
-  reader.onload = function(event) {
-    var content = event.target.result;
-    context = document.getElementById("imageDisplay").getContext("2d"),
-        img = new Image();
-    img.onload = function() {
-        context.drawImage(img, 300, 150);
-    };
-    img.src = content;
-    console.log(img);
-  }
-  reader.readAsDataURL(file);
+  let file = document.getElementById('fileDisplayArea').innerHTML;
+  let displayArea = document.getElementById('imageDisplay')
+  console.log(file, typeof(file));
 
-  reader.onerror = function(event) {
-    console.error('error, file could not be read: ' + event.target.error.code);;
-}
+  img = document.createElement('img');
+  img.src = "data:image/png;base64," + document.getElementById('fileDisplayArea').innerHTML;
+  displayArea.append(img);
+  // TODO get data via ajax to our DB our restful API
 }
 
 //turns an image into a blob
@@ -50,56 +36,7 @@ function imgToText() {
     }
     //read file as a bit string
     reader.readAsBinaryString(img);
-    //send via ajax to our DB via API
-
-    // this conditional isn't ideal, but works for MVP
-    // if(imgFile.type.slice(0,5) === 'image') {
-    //     console.log('hi');
-    //     var img = Image();
-    //     img.src = imgFile;
-    //     console.log(imgFile);
-    //     // console.log(imgFile);
-    //     // console.log(imgFile.type);
-    //     // let aFile = btoa(imgFile);
-    //     // console.log(aFile);
-    //     // let bFile = atob(aFile);
-    //     //$('#fileDisplayArea').append($('<img src=\'' + imgFile.name + '\'/>'));
-    //     //build json form for send via ajax
-    //     let $postData = {
-    //         'pic': imgFile.value,
-    //         'csrfmiddlewaretoken': $('[name="csrfmiddlewaretoken"]').val()
-    //     }
-    //     console.log($postData);
-        //ajax request to send image to DB
-        // $.ajax({
-        //     type: 'PATCH',
-        //     url: '/api/profile/8/',
-        //     data: $postData,
-        //     //contentType: 'application/json',
-        //     success: function(result) {
-        //         alert("success");
-        //     }
-    //     // });
-    // } else {
-    //     fileDisplayArea.innerText = "File not supported!";
-    // }
-    // fileElem.addEventListener('change', function(e) {
-    //   console.log('here');
-    //   let textType = /image.*/;
-    //   console.log(textType);
-    //     if (file.type.match(testType)) {
-    //         console.log('match');
-    //         var reader = new FileReader();
-    //
-    //         reader.onload = function(e) {
-    //             imgBlob = btoa($file);
-    //             console.log(imgBlob);
-    //             return imgBlob;
-    //         }
-    // } else {
-    //     fileDisplayArea.innerText = "File not supported!";
-    // }
-    // });
+    // TODO send data via ajax to our DB our restful API
 };
 
 //add click event so that image is processed upon submit
@@ -112,21 +49,3 @@ $('#showBtn').click(function(event) {
     event.preventDefault();
     textToImg();
 })
-
-// function toDataURL(url, callback) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.onload = function() {
-//     var reader = new FileReader();
-//     reader.onloadend = function() {
-//       callback(reader.result);
-//     }
-//     reader.readAsDataURL(xhr.response);
-//   };
-//   xhr.open('GET', url);
-//   xhr.responseType = 'blob';
-//   xhr.send();
-// }
-//
-// // toDataURL('', function(dataUrl) {
-//   console.log('RESULT:', dataUrl)
-// })
