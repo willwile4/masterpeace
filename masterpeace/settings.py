4e43002c4ac5b2d8d246ffaee01e9ef276c7d7af
Will Warren <will.warren04@gmail.com>
2017-05-08 00:25:54 -0400

    making secrets.py

--- masterpeace/settings.py
+++ masterpeace/settings.py
@@ -14,7 +14,7 @@ import os
 import dj_database_url
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-
+from . import settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
