f56ddd1a702774d39cb5d34b0c6dd2333f3a6707
Josh Friese <Friese@Friese.local>
2017-05-10 19:17:08 -0400

    fixed bug on profile creation

--- masterpeace/static/js/createProfile.js
+++ masterpeace/static/js/createProfile.js
@@ -3,19 +3,10 @@
 function createProfile(e) {
     e.preventDefault();
     var isChecked = $("#checkbox").prop("checked");
-    console.log('hi mom');
-    console.log(e);
-    let filename = $('#form-image').val()
-    for(var i = 0; i < filename.length; i++) {
-        if (filename[i] === '.') {
-            fileType = filename.slice([i+1]);
-        }
-    }
-
     let $form = {
         "user": $('[name="user_id"]').val(),
         'csrfmiddlewaretoken': $('[name="csrfmiddlewaretoken"]').val(),
-        'pic': "data:image/" + fileType + ";base64," + $("#form-image").attr('value'),
+        'pic': $("#form-image").attr('value'),
         'bio': $('[name="bio"]').val(),
         'fb_link': $('[name="fb_link"]').val(),
         'insta_link': $('[name="insta_link"]').val(),
